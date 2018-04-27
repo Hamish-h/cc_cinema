f9ece51b5c515cb38112278401b2f7b84ef260b6
Hanish-H <hamish.h@outlook.com>
2018-04-27 13:32:44 +0100

    updated class

--- db/console.rb
+++ db/console.rb
@@ -4,43 +4,43 @@ require_relative("../models/film.rb")
 require_relative("../models/ticket.rb")
 require_relative("../models/customer.rb")
 
-Film.delete_all()
-Ticket.delete_all()
-Customer.delete_all()
+# Film.delete_all()
+# Ticket.delete_all()
+# Customer.delete_all()
 
 Film1 = Film.new({
   "title" => "Doom",
-  "ticket_price" => 5.00
-})
-Film2.save()
-
-Film2 = Film.new({
-  "title" => "Star Wars, The Empire Strikes Back",
-  "ticket_price" => 5.00
-})
-Film3.save()
-
-Film3 = Film.new({
-  "title" => "Tron",
-  "ticket_price" => 5.00
+  "ticket_price" => 500
 })
 Film1.save()
 
+Film2 = Film.new({
+  "title" => "Star Wars, The Empire Strikes Back",
+  "ticket_price" => 500
+})
+Film2.save()
+
+Film3 = Film.new({
+  "title" => "Tron",
+  "ticket_price" => 500
+})
+Film3.save()
+
 customer1 = Customer.new({
   "name" => "William Goldman",
-  "funds" => 100
+  "funds" => 1000
 })
 customer1.save()
 
 customer2 = Customer.new({
   "name" => "S. Morgenstern",
-  "funds" => 100
+  "funds" => 1000
 })
 customer2.save()
 
 customer1 = Customer.new({
   "name" => "Inigo Montoya",
-  "funds" => 100
+  "funds" => 1000
 })
 customer3.save()
 

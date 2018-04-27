810d2be6e957d3982b6e64f953a8f73fe34b5654
Hanish-H <hamish.h@outlook.com>
2018-04-27 13:52:55 +0100

    added to ticket file and changed crud

--- models/ticket.rb
+++ models/ticket.rb
@@ -10,7 +10,23 @@ class Ticket
       @customer_id = options["customer_id"].to_i
     end
 
+  def save()
+      sql = "INSERT INTO tickets (customer_id, film_id) values ($1, $2) RETURNING id"
+      values = [@film_id, @customer_id]
+      ticket = SqlRunner.run(sql, values).first()
+      @id = ticket["id"].to_i
+    end
 
+    def delete()
+      sql = "DELETE FROM tickets WHERE id = $1;"
+      values = [@id]
+      SqlRunner.run(sql, values)
+    end
+
+    def self.delete_all()
+      sql = "DELETE FROM tickets;"
+      SqlRunner.run(sql)
+    end
 
 
 

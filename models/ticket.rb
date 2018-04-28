188838020727ed257d7c3710c9b4b6c0d7042afc
Hanish-H <hamish.h@outlook.com>
2018-04-28 18:31:07 +0100

    updated indentation

--- models/ticket.rb
+++ models/ticket.rb
@@ -5,10 +5,10 @@ class Ticket
   attr_accessor :film_id, :customer_id
 
   def initialize(options)
-      @id = options["id"].to_i if options["id"]
-      @film_id = options["film_id"].to_i
-      @customer_id = options["customer_id"].to_i
-    end
+    @id = options["id"].to_i if options["id"]
+    @film_id = options["film_id"].to_i
+    @customer_id = options["customer_id"].to_i
+  end
 
   def save()
     sql = "INSERT INTO tickets (customer_id, film_id) values ($1, $2) RETURNING id"
